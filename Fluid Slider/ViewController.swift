fa0ef8e1851842f22dc646401ff05d582264e9ad
Warren Hansen <whansen1@me.com>
2018-08-02 17:05:17 -0700

    initial commit

--- Fluid Slider/ViewController.swift
+++ Fluid Slider/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var mySegmentedSwitch: UISegmentedControl!
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // segmented switch
+        let lastSegment = defaults.integer(forKey: "segment")
+        print("Last segment was \(lastSegment)")
+        mySegmentedSwitch.selectedSegmentIndex = lastSegment
+        // slider
+        let lastSliderValue = defaults.float(forKey: "MaxDistance2")
+        let mStr = String(format: "%.1f", lastSliderValue)
+        print("Last slider value \(mStr)")
+        mySlider.setValue(lastSliderValue, animated: true)
+        myLabel.text = mStr
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sliderValueDidChange(_ sender: UISlider) {
+        let currentValue = sender.value
+        let mStr = String(format: "%.1f", currentValue)
+        print("New Slider Value \(mStr)")
+        defaults.set(currentValue, forKey: "MaxDistance2")
+        myLabel.text = mStr
     }
+    
+    @IBAction func switchDidChange(_ sender: AnyObject) {
 
-
+        switch mySegmentedSwitch.selectedSegmentIndex {
+        case 0:
+            defaults.set(0, forKey: "segment")
+            print("set \(0) and got back \(defaults.integer(forKey: "segment"))")
+        case 1:
+            defaults.set(1, forKey: "segment")
+            print("set \(1) got back \(defaults.integer(forKey: "segment"))")
+        case 2:
+            defaults.set(2, forKey: "segment")
+            print("set \(2) got back \(defaults.integer(forKey: "segment"))")
+        case 3:
+            defaults.set(3, forKey: "segment")
+            print("set \(3) got back \(defaults.integer(forKey: "segment"))")
+        default:
+            print("nothing selected")
+        }
+    }
+    
 }
 
